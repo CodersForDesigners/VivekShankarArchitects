1ed1d465cee31082377c0bc457235aee91144b61
adi <adityabhat@lazaro.in>
2019-02-12 15:57:16 +0530

    Data : Rename the database from `VSDP` to `VSA`

--- inc/db/setup.sql
+++ inc/db/setup.sql
@@ -1,6 +1,6 @@
 
--- Create a database called "VSDP" if it don't exist
+-- Create a database called "VSA" if it don't exist
 
-CREATE DATABASE IF NOT EXISTS VSDP CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS VSA CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE VSDP;
+USE VSA;
